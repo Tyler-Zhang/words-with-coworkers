294cbf7afc7218383a9fb91edcd87e1f59a77499
Tyler Zhang <tztylerzhang@gmail.com>
2018-06-25 22:24:49 -0400

    update help to include information about scrambling

--- src/app/slack/help.rs
+++ src/app/slack/help.rs
@@ -10,14 +10,15 @@ Here is how you use the scrabbler
 >`/scrabbler start [tags]` - Start a game with the person you're chatting to
 
 in game:
->`/scrabbler board` - show the state of the board
->`/scrabbler hand` - shows you your hand
->`/scrabbler play <word> <x>:<y> <right|down>` - to play a word
->`/scrabbler dict <word>` - to check if a word is valid
+>`/scrabbler board` - Show the state of the board
+>`/scrabbler hand` - Shows you your hand
+>`/scrabbler hand scramble` - Scrambles the letters in your hand
+>`/scrabbler play <word> <x>:<y> <right|down>` - To play a word
+>`/scrabbler dict <word>` - To check if a word is valid
 >`/scrabbler quit` - Quit the current game
 
 >When in a game, you must specify the the entirety of the word in the direction that you are playing
->For example: if {} is on the board and you want to play {}, you must specify the entire word 
+>For example: if {} is on the board and you want to play {}, you must specify the entire word
 >'fart' and not just 'art'
 ", translate_letters_to_emoji("F"), translate_letters_to_emoji("FART"));
 

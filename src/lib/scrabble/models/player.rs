ddd9aa0ea4d710936fb5f03f22e2195fda6a3d4a
Tyler Zhang <tztylerzhang@gmail.com>
2018-08-16 20:03:58 -0400

    player helper function and specs

--- src/lib/scrabble/models/player.rs
+++ src/lib/scrabble/models/player.rs
@@ -1,3 +1,5 @@
+use super::super::utils::string;
+
 pub struct Player {
     pieces: String
 }
@@ -5,5 +7,30 @@ pub struct Player {
 impl Player {
     pub fn new(pieces: String) -> Self {
         Player{ pieces }
+    }
+
+    pub fn remove_pieces(&mut self, pieces: &str) -> Result<(), String> {
+        self.pieces = string::remove_from_string(&self.pieces, pieces)?;
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_remove_pieces() {
+        let mut player = Player::new(format!("ABCDEFGH"));
+        assert_eq!(player.remove_pieces("ABDG").is_ok(), true);
+        assert_eq!(player.pieces, "CEFH");
+    }
+
+    #[test]
+    fn test_failed_remove_pieces() {
+        let mut player = Player::new(format!("ABCDEFGH"));
+        assert_eq!(player.remove_pieces("ABCIJK").is_err(), true);
+        assert_eq!(player.pieces, "ABCDEFGH");
     }
 }
